dd870d4f7f7006f237bdd019868d0e68eb3b973d
Jim Chen <jimmychimmyy@gmail.com>
2019-04-29 20:53:11 -0700

    separated home and demo pages

--- App/NewsFlash/app.js
+++ App/NewsFlash/app.js
@@ -5,6 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var demoRouter = require('./routes/demo');
 var usersRouter = require('./routes/users');
 
 var app = express();
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/demo', demoRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
