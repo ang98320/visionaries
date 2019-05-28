2775ebdbd687b7b00ab6f3ea4b5a53a50d6a250b
Shane Gilbert <sdgilber@ucsd.edu>
2019-05-28 13:46:46 -0700

    created new speed reader js for demo

--- App/NewsFlash/public/javascripts/speedreaderdemo.js
+++ App/NewsFlash/public/javascripts/speedreaderdemo.js
@@ -1,12 +1,35 @@
+// Google News API Key
+// a4a03d7d0df7480e8b52461a0e39fb77
+// Firebase API AIzaSyDYKhmDPqTGt1y52M1MI9VVnC0T6zXJML8
+
 //uncaught exception: [DEFAULT]: Firebase: No Firebase App '[DEFAULT]' has been created - call Firebase App.initializeApp() (app/no-app).
-text = 0
+data = 0
 articles = 0
 currentArticle = 0
+demo = 0  // will probably remove this later
 let wpm = 500;
 
-$(document).ready(function() {
-  console.log("hello from speedreaderdemo.js");
-  });
+$(document).ready(function(){
+  console.log("hello from speedreader.js");
+});
+
+function getTrendingNews(callback) {
+  var url = 'https://newsapi.org/v2/top-headlines?' +
+          'country=us&' +
+          'apiKey=a4a03d7d0df7480e8b52461a0e39fb77';
+  var req = new Request(url);
+  fetch(req)
+      .then(function(response) {
+          //console.log(response.json());
+          return response.json();
+      })
+      .then(function(json) {
+        data = json
+  //use variable articles from here on to access articles, as savedarticle.js doesn't need to use the data variable in its context
+  articles = data.articles
+        callback();
+      })
+}
 
 function demoSave() {
   var xhttp = new XMLHttpRequest();
@@ -24,24 +47,29 @@ function demoAjax() {
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
      currentArticle = "article-button-demo";
-     text = this.responseText;
-     console.log(text);
+     demo = this.responseText;
     }
   };
   xhttp.open("GET", "/demo-speedreader", true);
   xhttp.send();
 }
 
+function initReader(id) {
+  console.log(id);
+  currentArticle = id;
+}
+
 let speedreader = 0
 //function readText(text) {
 function startReader() {
+  text = demo;
+
   if(text == null)
     text = "Article_Text_Not_Found!";
   //if a speedreader interval is already open, close it and start a new one
   if (speedreader)
-	closeReader();
+  closeReader();
 
-  console.log(text);
   let words = text.split(" ");
   let numWords = words.length - 1;
   let index = 0;
